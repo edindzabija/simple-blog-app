2a73b29d6a5ad739bfacbe751f9db79f97d6c7d7
Edin Dzabija <22686545+edindzabija@users.noreply.github.com>
2025-09-28 03:56:21 +0200

    added home button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,12 @@ export default function Header() {
           </Link>
           <nav className="flex gap-4">
             <Link
+              href="/"
+              className="px-4 py-2 bg-gray-900 text-white rounded hover:bg-gray-800 focus:ring-gray-900 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
+            >
+              Home
+            </Link>
+            <Link
               href="/new"
               className="px-4 py-2 bg-gray-900 text-white rounded hover:bg-gray-800 focus:ring-gray-900 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2"
             >
